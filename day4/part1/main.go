3b1d8213a2e2c07982d42017d1adc799fb453712
Guillermo Fernández <bengallme@gmail.com>
2024-12-05 17:52:05 -0600

    solved day 4 part 1

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -7,71 +7,97 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("../test-input")
+	data, err := os.ReadFile("../input")
 	if err != nil {
 		panic(err)
 	}
 
-	stringMatrix := string(data)
-	fmt.Println(countVertical(stringMatrix))
+	mat := [][]string{}
 
-}
-
-func countHorizontal(strMat string) int {
-	count := 0
-	lines := strings.Split(strMat, "\n")
-
-	for _, l := range lines {
-		count += countSub(l, "XMAS")
-		count += countSub(reversed(l), "XMAS")
+	for _, l := range strings.Split(string(data), "\n") {
+		row := strings.Split(l, "")
+		if len(row) == 0 {
+			continue
+		}
+		mat = append(mat, row)
 	}
 
-	return count
-}
-func countVertical(strMat string) int {
 	count := 0
 
-	rows := strings.Split(strMat, "\n")
-
-	columns := []string{}
-	colBytes := make([][]byte, len(rows[0]))
-
-	for _, r := range rows {
-		for i := range len(r) {
-			colBytes[i] = append(colBytes[i], r[i])
+	for i := range len(mat) {
+		for j := range len(mat[0]) {
+			if mat[i][j] == "X" {
+				count += checkAndCount(mat, i, j)
+			}
 		}
 	}
 
-	for _, colB := range colBytes {
-		columns = append(columns, string(colB))
-	}
+	fmt.Println(count)
+}
 
-	for _, c := range columns {
-		count += countSub(c, "XMAS")
-		count += countSub(reversed(c), "XMAS")
-	}
+func checkAndCount(mat [][]string, i, j int) int {
 
+	count := 0
+	rowLength := len(mat[0])
+	colLength := len(mat)
+
+	// right
+	if j < rowLength-3 && strings.Join(mat[i][j:j+4], "") == "XMAS" {
+		count++
+	}
+	// left
+	if j > 2 && reversed(strings.Join(mat[i][j-3:j+1], "")) == "XMAS" {
+		count++
+	}
+	// up
+	if i > 2 && reversed(joinCol(mat, j, i-3, i+1)) == "XMAS" {
+		count++
+	}
+	// down
+	if i < colLength-3 && joinCol(mat, j, i, i+4) == "XMAS" {
+		count++
+	}
+	// right+down
+	if j < rowLength-3 && i < colLength-3 && joinDiagonal(mat, i, j, 1, 1) == "XMAS" {
+		count++
+	}
+	// right+up
+	if j < rowLength-3 && i > 2 && joinDiagonal(mat, i, j, -1, 1) == "XMAS" {
+		count++
+	}
+	// left+down
+	if j > 2 && i < colLength-3 && joinDiagonal(mat, i, j, 1, -1) == "XMAS" {
+		count++
+	}
+	// left+up
+	if j > 2 && i > 2 && joinDiagonal(mat, i, j, -1, -1) == "XMAS" {
+		count++
+	}
 	return count
-}
 
-func countDiagonal(strMat string) {
-
-}
-
-func countSub(str, sub string) int {
-	sum := 0
-	for i := range len(str) - len(sub) + 1 {
-		if str[i:i+len(sub)] == sub {
-			sum++
-		}
-	}
-	return sum
 }
 
 func reversed(str string) string {
-	new := []byte{}
+	final := []byte{}
 	for i := len(str) - 1; i >= 0; i-- {
-		new = append(new, str[i])
+		final = append(final, str[i])
+
 	}
-	return string(new)
+	return string(final)
+}
+
+func joinCol(mat [][]string, colIndex, start, finish int) string {
+	final := ""
+	for i := start; i < finish; i++ {
+		final += mat[i][colIndex]
+	}
+	return final
+}
+func joinDiagonal(mat [][]string, rowIndex, colIndex, rowDirection, colDirection int) string {
+	final := ""
+	for i := range 4 {
+		final += mat[rowIndex+(rowDirection*i)][colIndex+(colDirection*i)]
+	}
+
+	return final
 }
