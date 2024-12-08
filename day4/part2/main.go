a5aeb47930702a85d1ed98314b52f0420f8d54c6
Guillermo Fernández <bengallme@gmail.com>
2024-12-08 16:29:22 -0600

    solved day 4 part 2

--- day4/part2/main.go
+++ day4/part2/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("../test-input")
+	data, err := os.ReadFile("../input")
 	if err != nil {
 		panic(err)
 	}
@@ -36,52 +36,29 @@ func main() {
 }
 
 func checkAndCount(mat [][]string, i, j int) int {
-
-	count := 0
 	rowLength := len(mat[0])
 	colLength := len(mat)
 
-	// right+down
-	if j < rowLength-3 && i < colLength-3 && joinDiagonal(mat, i, j, 1, 1) == "XMAS" {
-		count++
-	}
-	// right+up
-	if j < rowLength-3 && i > 2 && joinDiagonal(mat, i, j, -1, 1) == "XMAS" {
-		count++
-	}
-	// left+down
-	if j > 2 && i < colLength-3 && joinDiagonal(mat, i, j, 1, -1) == "XMAS" {
-		count++
-	}
-	// left+up
-	if j > 2 && i > 2 && joinDiagonal(mat, i, j, -1, -1) == "XMAS" {
-		count++
-	}
-	return count
-
-}
-
-func reversed(str string) string {
-	final := []byte{}
-	for i := len(str) - 1; i >= 0; i-- {
-		final = append(final, str[i])
-
-	}
-	return string(final)
-}
-
-func joinCol(mat [][]string, colIndex, start, finish int) string {
-	final := ""
-	for i := start; i < finish; i++ {
-		final += mat[i][colIndex]
-	}
-	return final
-}
-func joinDiagonal(mat [][]string, rowIndex, colIndex, rowDirection, colDirection int) string {
-	final := ""
-	for i := range 4 {
-		final += mat[rowIndex+(rowDirection*i)][colIndex+(colDirection*i)]
+	diagOne := false
+	diagTwo := false
+	if j > 0 && j < rowLength-1 && i > 0 && i < colLength-1 {
+		if mat[i-1][j-1] == "M" && mat[i+1][j+1] == "S" {
+			diagOne = true
+		}
+		if mat[i-1][j-1] == "S" && mat[i+1][j+1] == "M" {
+			diagOne = true
+		}
+		if mat[i-1][j+1] == "M" && mat[i+1][j-1] == "S" {
+			diagTwo = true
+		}
+		if mat[i-1][j+1] == "S" && mat[i+1][j-1] == "M" {
+			diagTwo = true
+		}
 	}
 
-	return final
+	if diagOne && diagTwo {
+		return 1
+	}
+
+	return 0
 }
